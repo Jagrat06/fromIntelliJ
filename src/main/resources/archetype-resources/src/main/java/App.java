e7ffa1c4945a218f1723b8829d8cd34b9729b2f9
Jagrat06 <jagratsahoo@gmail.com>
2022-08-24 19:12:43 +0530

    send from intelliJ UI

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.example;
+
+/**
+ * Hello world!
+ *Hi just for testing the feike
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
